710a023b594bcc306e493987513acc08b5f281eb
amit19-meet <amit19@meet.mit.edu>
2017-07-27 12:12:41 +0300

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,20 @@
+ # first, we run the loop. so we will see numbers from 1 to 100.
+        # we also worte that every time another number from the range shows up,
+        # we add it to c. so c, which is 0 will end up being 5050.
+
+def add_numbers(333, 777):
+    c = 0
+    for number in range(1, 100 + 1):
+##        print(number)
+        c = c + number
+    return(c)
+
+answer = add_numbers()
+print(answer)
+
+
+
+
+    
+        
+
